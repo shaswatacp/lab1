b3ba5e975a0ba49e1e35bfb91467e99b5d0abfdb
Shaswata Chowdhury <140137696+shaswatacp@users.noreply.github.com>
2023-09-30 08:50:07 +0600

    Add files via upload

---
+++ prac.01.c
@@ -0,0 +1,10 @@
+//Write a c program to print your address and personal information.
+#include<stdio.h>
+int main()
+{
+    printf("Name : Shaswata Chowdhury\n");
+    printf("ID:2108036\n");
+    printf("Dept: ETE\n");
+    printf("Address:CUET\n");
+    return 0;
+}

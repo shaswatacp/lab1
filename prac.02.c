d522f435bdd00baf30b21ec0dac341f35438e568
Shaswata Chowdhury <140137696+shaswatacp@users.noreply.github.com>
2023-09-30 09:10:09 +0600

    Add files via upload

---
+++ prac.02.c
@@ -0,0 +1,11 @@
+//Write a c program to calculate the area of a circle.
+#include<stdio.h>
+int main()
+{
+    float r,area;
+    r=5;
+    area=3.1416*r*r;
+    printf("The area of the circle is %f\n",area);
+    return 0;
+}
+

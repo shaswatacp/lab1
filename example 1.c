47123fd119bf1adecb82ac6ae33bc750e3d418bb
Shaswata Chowdhury <140137696+shaswatacp@users.noreply.github.com>
2023-10-21 19:16:45 +0600

    Add files via upload

---
+++ example 1.c
@@ -0,0 +1,14 @@
+//A c program to display a persons information.
+#include<stdio.h>
+int main()
+{
+
+
+    printf("Name:Shaswata Chowdhury\n");
+    printf("Id:2108036\n");
+    printf("Dept:ETE\n");
+    printf("Adress:23 no.gura mia chowdhury lane,chattogram");
+    return 0;
+
+
+}

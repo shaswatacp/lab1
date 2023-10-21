f73b4d3ccfc7ec8e563393354a6d2fcaa27271c1
Shaswata Chowdhury <140137696+shaswatacp@users.noreply.github.com>
2023-10-21 19:25:54 +0600

    Add files via upload

---
+++ example2.c
@@ -0,0 +1,11 @@
+//A program to add to integer numbers.
+#include<stdio.h>
+int main()
+{
+    int a,b,sum;
+    a=5;
+    b=7;
+    sum=a+b;
+    printf("summation of %d and %d=%d\n",sum);
+    return 0;
+}
